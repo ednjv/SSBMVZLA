9676c771c197899bda58a44f1664e55dc48a3625
Edwin Vasquez <edwinvasquez50@gmail.com>
2016-10-29 21:33:39 -0300

    Fixed index error

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 // change the following paths if necessary
 $yii=dirname(__FILE__).'/../yii-1.1.16.bca042/framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/main.php';
-if ($_SERVER['HTTP_HOST']) === "www.ssbmvenezuela.byethost11.com") {
+if ($_SERVER['HTTP_HOST'] === "www.ssbmvenezuela.byethost11.com") {
 	$config=dirname(__FILE__).'/protected/config/main.production.php';
 }
 
